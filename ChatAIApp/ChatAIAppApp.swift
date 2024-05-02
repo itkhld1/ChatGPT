d7eee14127d59ce187b117dba64272839b5d901a
Khaled Samim <asamim20@posta.pau.edu.tr>
2024-05-02 15:45:41 +0300

    ChatGPT

---
+++ ChatAIApp/ChatAIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIAppApp.swift
+//  ChatAIApp
+//
+//  Created by itkhld on 2.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
